8a87ce464093d057770c248f67613d0c021d3994
Andrew Guo <andrewguo@Ges-MacBook-Pro-5.local>
2019-09-22 09:32:05 +1000

    Added redux-devtools-extension package and start to initilise src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import App from "./containers/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
